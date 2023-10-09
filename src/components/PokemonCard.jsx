73612c31e5b50c8c46987248280315a9a91759f7
27rachiid82 <mohammedikacirachid@yahoo.fr>
2023-10-09 17:04:20 +0200

    ajout 2nd pokemon + visuel sur img ou non

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,42 @@
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+
+
+  ];
+
+
 function PokemonCard () {
-    return (
-        <figure>
+
+        const pokemon = pokemonList[1];
+
+       if (pokemon.imgSrc != null) {
+            return<figure>
             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
             <figcaption>"Bulbasaur"</figcaption>
         </figure>
-    );
 
-}
+       }
+        else {
+            return(<div><p>???</p><p>Mew</p></div>)
+       }
+
+
+     return (
+         <figure>
+             <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
+             <figcaption>"Bulbasaur"</figcaption>
+         </figure>
+     );
+    }
+     
 
 export default PokemonCard;
+
