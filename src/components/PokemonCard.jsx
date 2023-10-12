45ebfda16f8cd6156e38be530f648e6181f5ff5e
27rachiid82 <mohammedikacirachid@yahoo.fr>
2023-10-12 10:24:57 +0200

    test props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,32 +1,45 @@
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
+
+  //const pokemonList = 
+  //[
+      //{
+        //name:"bulbasaur",
+        //imgSrc:
+          //"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      //},
+      //{
+        //name: "mew",
+      //},
+  
+  
+    //];
+
+function PokemonCard (props) {
+
+console.log(props)
+
+       if (props.pokemon.imgSrc != null) 
+       {
+            return  (
+            
+
+              <figure>
+              
+              <img src={props.pokemon.imgSrc} alt={props.pokemon.name}/>
+              <figcaption>{props.pokemon.name}</figcaption>
+
+                
+              </figure>)
 
 
-  ];
 
+        }
+          else 
+        {
+            return(<div><p>???</p><figcaption>"Mew"</figcaption></div>)
+        }
 
-function PokemonCard () {
-
-        const pokemon = pokemonList[1];
-
-       if (pokemon.imgSrc != null) {
-            return<figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>"Bulbasaur"</figcaption>
-        </figure>
-
-       }
-        else {
-            return(<div><p>???</p><p>Mew</p></div>)
-       }
+      
 
 
      return (
@@ -35,8 +48,10 @@ function PokemonCard () {
              <figcaption>"Bulbasaur"</figcaption>
          </figure>
      );
-    }
+
+
      
+    } 
 
 export default PokemonCard;
 
